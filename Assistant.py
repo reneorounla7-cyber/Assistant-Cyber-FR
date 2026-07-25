d5d2e33592075051d4b982cc64ae5732f2822d63
reneorounla7-cyber <reneorounla7@gmail.com>
2026-07-25 08:09:52 +0100

    Create Assistant.py

---
+++ Assistant.py
@@ -0,0 +1,28 @@
+print("🛡️ Bienvenue dans Assistant Cyber FR")
+
+while True:
+    print("\n===== MENU =====")
+    print("1. Vérifier un mot de passe")
+    print("2. Générer un mot de passe")
+    print("3. Conseils de cybersécurité")
+    print("4. Quitter")
+
+    choix = input("Choisissez une option : ")
+
+    if choix == "1":
+        print("Fonction bientôt disponible.")
+
+    elif choix == "2":
+        print("Fonction bientôt disponible.")
+
+    elif choix == "3":
+        print("Ne partagez jamais votre mot de passe.")
+        print("Activez l'authentification à deux facteurs.")
+        print("Mettez régulièrement vos logiciels à jour.")
+
+    elif choix == "4":
+        print("Au revoir !")
+        break
+
+    else:
+        print("Choix invalide.")
